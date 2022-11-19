dd029ae3fc34df9740efa94c513199d14040c180
Johaan-Paul <johaanp2008@gmail.com>
2022-11-19 23:57:02 +1300

    Update ComputerBase.js

--- Page_Projects/Projects/Epic-Archery/js/ComputerBase.js
+++ Page_Projects/Projects/Epic-Archery/js/ComputerBase.js
@@ -7,7 +7,7 @@ class ComputerBase {
     this.body = Bodies.rectangle(x, y, width, height, options);
     this.width = width;
     this.height = height;
-    this.image = loadImage("./assets/base2.png");
+    this.image = loadImage("assets/base2.png");
 
     World.add(world, this.body);
   }
