1d48c5b556969579bfb915bb0ec6bb536ef73d35
Johaan-Paul <johaanp2008@gmail.com>
2022-11-19 23:59:35 +1300

    Update sketch.js

--- Page_Projects/Projects/Epic-Archery/sketch.js
+++ Page_Projects/Projects/Epic-Archery/sketch.js
@@ -13,7 +13,7 @@ var playerArcherLife = 3;
 var computerArcherLife = 3;
 var computerCollision
 function preload() {
-  backgroundImg = loadImage("/assets/background.gif");
+  backgroundImg = loadImage("assets/background.gif");
 }
 
 function setup() {
